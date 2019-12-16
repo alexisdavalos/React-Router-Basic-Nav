589fd0e60c308de395b701f3706d79954ffe3fe3
Alexis <alexisdavalos.tech@gmail.com>
2019-12-16 17:42:06 -0500

    Set up Nav and rendered componenets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {Route, Switch} from 'react-router-dom';
 
 const App = () => (
   <div>
     <Navigation />
+    {/* Set Up Routes */}
+    <Switch>
+      <Route path='/about'>
+        <About/>
+      </Route>
+      <Route path='/contact'>
+        <Contact/>
+      </Route>
+      <Route exact path='/'>
+        <Home/>
+      </Route>
+    </Switch>
   </div>
 );
 
